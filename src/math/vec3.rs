7c0c609b2f8d5f0935f7f5eda03c6a85b310b12f
max-jellycat <max@jellycat.fr>
2021-04-23 21:33:23 +0200

    Vector3 and Point

---
+++ src/math/vec3.rs
@@ -0,0 +1,29 @@
+pub struct Vec3 {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+
+    pub fn zero() -> Vec3 {
+        Vec3::new(0.0, 0.0, 0.0)
+    }
+
+    pub fn one() -> Vec3 {
+        Vec3::new(1.0, 1.0, 1.0)
+    }
+
+    pub fn x(&self) -> f32 {
+        self.x
+    }
+    pub fn y(&self) -> f32 {
+        self.y
+    }
+    pub fn z(&self) -> f32 {
+        self.z
+    }
+}
