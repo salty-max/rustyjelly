469d732d75f53af1e510bc0b56d180e2bfc78c78
max-jellycat <max@jellycat.fr>
2021-04-24 00:20:02 +0200

    Transform mod

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,7 +1,9 @@
 pub mod matrix4x4;
+pub mod transform;
 pub mod vec3;
 
 pub mod prelude {
     pub use crate::math::matrix4x4::*;
+    pub use crate::math::transform::*;
     pub use crate::math::vec3::*;
 }
---
+++ src/math/transform.rs
@@ -0,0 +1,26 @@
+use super::{matrix4x4::Matrix4x4, prelude::Vec3};
+
+#[derive(Debug, Copy, Clone)]
+pub struct Transform {
+    pub position: Vec3,
+    pub rotation: Vec3,
+    pub scale: Vec3,
+}
+
+impl Transform {
+    pub fn new() -> Transform {
+        Transform {
+            position: Vec3::zero(),
+            rotation: Vec3::zero(),
+            scale: Vec3::one(),
+        }
+    }
+
+    pub fn get_transformation_matrix(&self) -> Matrix4x4 {
+        let translation = Matrix4x4::translation(self.position);
+        let rotation = Matrix4x4::rotation(self.rotation);
+        let scale = Matrix4x4::scale(self.scale);
+
+        translation * rotation * scale
+    }
+}
