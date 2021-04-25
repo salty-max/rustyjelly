f21cb80c557165c3050e15fc0388cd1318d442de
max-jellycat <max@jellycat.fr>
2021-04-25 03:00:47 +0200

    Component + EntityBuilder

--- jelly_engine/src/core/mod.rs
+++ jelly_engine/src/core/mod.rs
@@ -1,6 +1,8 @@
+mod component;
 pub mod entity;
 mod scene;
 mod world;
 
+pub use component::Component;
 pub use scene::{Scene, Transaction};
 pub use world::World;
--- jelly_engine/src/core/world.rs
+++ jelly_engine/src/core/world.rs
@@ -1,12 +1,24 @@
-use super::entity::{Entities, Entity};
+use super::{
+    component::{Component, Components},
+    entity::{Entities, Entity, EntityBuilder},
+};
 
 #[derive(Debug, Default)]
 pub struct World {
     entities: Entities,
+    components: Components,
 }
 
 impl World {
-    pub fn build_entity(&mut self) -> Entity {
-        self.entities.create_entity()
+    pub fn build_entity(&mut self) -> EntityBuilder {
+        EntityBuilder::new(self.entities.create_entity(), self)
+    }
+
+    pub fn delete_entity(&mut self, entity: &Entity) {
+        self.components.delete_entity(entity);
+    }
+
+    pub fn insert_component<C: Component>(&mut self, entity: &Entity, component: C) {
+        self.components.insert(entity, component);
     }
 }
